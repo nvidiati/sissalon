df2973a68c7a428907016bf95fd33e25dfed1cb9
Kriz <nvidiatib5@gmail.com>
2025-01-23 16:40:47 -0400

    Go back

--- resources/views/admin/pos/create.blade.php
+++ resources/views/admin/pos/create.blade.php
@@ -258,7 +258,6 @@
                                 <tbody>
                                     <tr id="no-service">
                                         <td colspan="5" class="text-center text-danger">@lang("messages.addService")</td>
-                                        <td colspan="5" class="text-center text-danger">@lang("messages.addService")</td>
                                     </tr>
                                 </tbody>
                             </table>
