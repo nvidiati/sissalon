2ffe4ca271ced854bb041ef11ecea3a225a3bacf
Kriz <nvidiatib5@gmail.com>
2025-03-24 16:23:57 -0400

    ..

--- app/Http/Controllers/Admin/BookingController.php
+++ app/Http/Controllers/Admin/BookingController.php
@@ -224,7 +224,7 @@ class BookingController extends AdminBaseController
                     }
 
                     //crisobal
-                    $action .= ' <a href="''" data-id="'. $row->id .'" class="print-reservation btn btn-warning btn-circle "
+                    $action .= ' <a href="" data-id="'. $row->id .'" class="print-reservation btn btn-warning btn-circle "
                     data-toggle="tooltip" data-original-title="Imprimir"><i class="fa fa-btn-print" aria-hidden="true"></i></a> ';
 
                     $action .= ' <a href="'.route('admin.bookings.show', [$row->id]).'" data-booking-id="' . $row->id . '" class="btn btn-info btn-circle "
